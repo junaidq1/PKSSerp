2f696ea4e61d17940eb349f01a40ba8bae41bd5f
Lal Zada <lalzadamohmand@gmail.com>
2017-01-04 00:57:33 +0500

    student and attendance date fields disabled

--- src/attendance/forms.py
+++ src/attendance/forms.py
@@ -1,15 +1,16 @@
-
 from django import forms
-import datetime
-#from django.contrib.admin.widgets import AdminDateWidget
-from django.forms import ModelForm
-
 from .models import Attendance
 
 
 class AttendanceForm(forms.ModelForm):
-	class Meta:
-		model = Attendance
-		fields = ("student","attendance_date", "status", "notes")
 
+    attendance_date = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+
+    class Meta:
+        model = Attendance
+        fields = ("student", "attendance_date", "status", "notes")
+
+    def __init__(self, *args, **kwargs):
+        super(AttendanceForm, self).__init__(*args, **kwargs)
+        self.fields['student'].widget.attrs['disabled'] = 'true'
 
